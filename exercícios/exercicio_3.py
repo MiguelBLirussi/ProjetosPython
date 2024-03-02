88bf7de37ae3c6e81cef8a8c1bc1386d53a3ed66
muskito-ux <miguelbarreto.costa2@gmail.com>
2024-03-02 10:57:05 -0300

    ProjetosPython

--- exercícios/exercicio_3.py
+++ exercícios/exercicio_3.py
@@ -5,12 +5,6 @@ notas = []
 while len(notas) < 4:
     numero = float(input('- Digite uma nota: '))
     notas.append(numero)
-<<<<<<< HEAD
 
 media = (sum(notas) / len(notas))
 print(f'- Média das notas: {media}')
-=======
-a = 1
-media = (sum(notas) / len(notas))
-print(f'- Média das notas: {media}')
->>>>>>> ffe2988a1dafa52d36d5f7393c90294a2dec8fcb
