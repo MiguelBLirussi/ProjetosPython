baef0340fdfb31a2fe3aea8a1712c31e760c6f7d
muskito-ux <miguelbarreto.costa2@gmail.com>
2024-03-02 10:50:22 -0300

    ProjetosPython

--- exercícios/exercicio_3.py
+++ exercícios/exercicio_3.py
@@ -1,16 +1,10 @@
 # Faça um Programa que leia 4 notas, mostre as notas e a média na tela.
 
-lista = []
+notas = []
 
-while len(lista) < 4:
-    numero = float(input('- Digite um número: '))
-    lista.append(numero)
+while len(notas) < 4:
+    numero = float(input('- Digite uma nota: '))
+    notas.append(numero)
 
-nota1 = lista[0]
-nota2 = lista[1]
-nota3 = lista[2]
-nota4 = lista[3]
-divisor = len(lista)
-soma_notas = nota1 + nota2 + nota3 + nota4
-media = (soma_notas / divisor)
+media = (sum(notas) / len(notas))
 print(f'- Média das notas: {media}')
