129e852f1cd22c33b9bf4269ada2d352f9976e90
muskito-ux <miguelbarreto.costa2@gmail.com>
2024-03-06 23:20:31 -0300

    ProjetosPython

--- jogos/forca.py
+++ jogos/forca.py
@@ -1,29 +1,45 @@
+import random
+
 def jogar():
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    palavras = ['maça','banana','abacaxi','abacate','pera','kiwi','laranja','limão','caju','manga','uva',]
+    num_palavra = random.randint(0,10)
+    print(num_palavra)
+
+    palavra_secreta = palavras[num_palavra].upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
-
+    erros = 0
+    total_erros = letras_acertadas.count("_")
     print(letras_acertadas)
 
     while(not enforcou and not acertou):
 
         chute = input("Qual letra? ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        index = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-            index = index + 1
-
+        if (chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+            print(f'\nA palavra não possui a letra {chute}\nErros: {erros}/{total_erros}\n')
+        
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
-
+        if acertou:
+            print('Parabéns!!! Você Ganhou!!\n')
+        if enforcou:
+            print('Você Perdeu!! :(\nTente Novamente!!')
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
